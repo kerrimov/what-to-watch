cb49c2f4c83924fc6e69a439412810bdb2d34709
dima-diachkov <diachkov.dima@gmail.com>
2022-05-24 21:38:23 +0300

    expanded readme, returned the tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 function App() {
   return (
     <div>
-      <h1>What to watch</h1>
+      <h1>
+        What to watch
+      </h1>
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div>
+//         <h1>
+//           Learn React
+//         </h1>
+//     </div>
+//   );
+// }
 
 export default App;

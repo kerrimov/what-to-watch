2383df7819dd14a5c78e98c5c535851404ca1717
Eugen Kakareka <evv.kakareka@gmail.com>
2022-06-15 10:07:45 +0300

    add index to root of each component

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Search } from "./Search/Search";
-import { SecondaryButton } from "../../shared/SecondaryButton/SecondaryButton";
+import { Search } from "./Search";
+import { SecondaryButton } from "../../shared/SecondaryButton";
 import { Box } from "@mui/material";
 import "./SearchField.scss";
 

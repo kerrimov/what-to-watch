2ec372d3aa6c162c91a8492e2333581777bb3976
Maria Khudolii <marichka0572@gmail.com>
2022-06-16 10:43:59 +0300

    routing set up

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom"; 
 import { ThemeProvider } from "@mui/material";
 import App from "./App";
 import customTheme from "./theme/customTheme";
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={customTheme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter> 
     </ThemeProvider>
   </React.StrictMode>,
 );

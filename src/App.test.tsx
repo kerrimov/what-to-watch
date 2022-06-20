edc877debdcd2bf8da556724e557f528dbe5dd54
Maria Khudolii <marichka0572@gmail.com>
2022-06-20 12:30:00 +0300

    App.test changed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/What to watch/i);
-  expect(linkElement).toBeInTheDocument();
+test("render without crashing", () => {
+  const div = document.createElement("div");
+  const root = createRoot(div);
+  root.render(<App />);
+  root.unmount();
 });

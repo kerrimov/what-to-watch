91c6cd596f418d8d8d0f6057c226be2374284dc0
Maria Khudolii <marichka0572@gmail.com>
2022-06-21 11:28:27 +0300

    router component added

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom"; 
 import { ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import customTheme from "./theme/customTheme";
-
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={customTheme}>
       <BrowserRouter>
         <App />
-      </BrowserRouter> 
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );

5f8469f2b0de569c5f580e0e474460e18e42b944
ananaple <rage.as.sage@gmail.com>
2022-06-24 15:52:33 +0300

    commiting router, redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { CardContainer } from "./components/CardContainer/CardContainer";
 import { Routes, Route } from "react-router-dom";
 import { ActorsPageContainer } from "./components/ActorsPageContainer";
-import { ActorCard } from "./components/ActorsPageContainer/ActorCard";
+// import { ActorCard } from "./components/ActorsPageContainer/ActorCard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<CardContainer />} />
       <Route path="/people" element={<ActorsPageContainer />} />
-      <Route path="/people/:id" element={<ActorCard />} />
+      {/* <Route path="/people/:id" element={<ActorCard name={""} image={""} lastActing={""} />} /> */} {/*reserved place for actorfullinfo*/}
     </Routes>
   );
 }

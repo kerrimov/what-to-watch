94aeed4875c25350c72cdf8657c4d74237fb93a2
Maria Khudolii <marichka0572@gmail.com>
2022-06-27 12:15:18 +0300

    search field added to header

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -4,9 +4,11 @@ import { SecondaryButton } from "../../shared/SecondaryButton";
 import { Box } from "@mui/material";
 import "./SearchField.scss";
 
+
 export const SearchField: React.FC = () => (
   <Box className="search">
     <Search />
     <SecondaryButton>Search</SecondaryButton>
   </Box>
 );
+

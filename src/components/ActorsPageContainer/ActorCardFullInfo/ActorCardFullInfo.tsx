9aee38efbb51912c257657a1c60be94a2551cca9
ananaple <rage.as.sage@gmail.com>
2022-06-28 16:50:22 +0300

    added ActorsPageFullInfo component, fixed some comments

--- src/components/ActorsPageContainer/ActorCardFullInfo/ActorCardFullInfo.tsx
+++ src/components/ActorsPageContainer/ActorCardFullInfo/ActorCardFullInfo.tsx
@@ -1,35 +1,84 @@
 import * as React from "react";
-import {  Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
-import { useNavigate } from "react-router";
+import { Avatar, Box, Typography, Button } from "@mui/material";
+import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { API_TOKEN } from "../../../shared/api/api";
 
-interface Props {
-  id: number;
-  name: string;
-  img: string;
-  knownFor: string;
+interface Actor {
+  actor: {
+    id: number;
+    name: string;
+    birthday: string;
+    known_for_department: string;
+    place_of_birth: string;
+    profile_path: string;
+    biography: string;
+    popularity: string;
+  }
 }
 
-export const ActorCard = ({id, name, img, knownFor}: Props) => {
+const styles = {
+  wrapper: {
+    display: "flex",
+    margin: "10px 10px 10px 10px",
+    color: "primary.dark",
+  },
+  leftSideBar: {
+    display: "flex", 
+    flexDirection: "column",
+  },
+  main: {
+    width: "70%",
+  },
+  avatar: {
+    width: 400,
+    height: 600,
+    borderRadius: "10px",
+    margin: "0 20px 20px 0",
+  },
+  heading: {
+    paddingTop: "20px",
+  }
+};
 
-  const navigate = useNavigate();
+
+export const ActorCardFullInfo = () => {
+
+  const [actor, setActor] = useState<any>();
+  const [showMore, setShowMore] = useState<boolean>(false);
+  const actorId = useParams();
+  
+  useEffect(() => {
+    axios.get(`https://api.themoviedb.org/3/person/${actorId.id}?api_key=${API_TOKEN}`)
+      .then(res=>{
+        setActor(res.data);
+      }); 
+  }, []);
+  
+  console.log(actor);
   return (
-    <Card sx={{ maxWidth: 300 }} onClick={()=>navigate(`/person/${id}`)}>
-    <CardActionArea>
-      <CardMedia
-        component="img"
-        height="140"
-        image={img}
-        alt={name}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {knownFor}
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-  </Card>
+      <Box sx={styles.wrapper}>
+        <Box sx={styles.leftSideBar}>
+            <Avatar src={`https://image.tmdb.org/t/p/w500/${actor?.profile_path}`} alt={actor?.name} variant="square" sx={styles.avatar}/>
+            <Typography variant="h4">Personal Info</Typography>
+            <Typography variant="h5" sx={styles.heading}>Known For</Typography>
+            <Typography variant="h6">{actor?.known_for_department}</Typography>
+            <Typography variant="h5" sx={styles.heading}>Birthday</Typography>
+            <Typography variant="h6">{actor?.birthday}</Typography>
+            <Typography variant="h5" sx={styles.heading}>Place of Birth</Typography>
+            <Typography variant="h6">{actor?.place_of_birth}</Typography>
+            <Typography variant="h5" sx={styles.heading}>Popularity</Typography>
+            <Typography variant="h6">{actor?.popularity}</Typography>
+        </Box> 
+         <Box sx={styles.main}>
+            <Typography variant="h3">{actor?.name}</Typography>
+            <Typography variant="h5" sx={styles.heading}>Biography</Typography>
+            <Typography>{ showMore ? actor?.biography: actor?.biography.slice(0, 400) + "..." }</Typography>
+              { !showMore &&
+                <Button onClick={()=>{setShowMore(true);}}>Show more &gt;</Button>
+              } 
+        </Box>
+      </Box>
   );
 };
